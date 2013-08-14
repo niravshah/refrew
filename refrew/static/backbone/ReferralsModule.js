b250e230303c972b0c7f6cebf2636242dce1a695
niravshah <nirav.shah83@gmail.com>
2013-08-14 15:34:12 +0000

    Removed Spinjs

--- refrew/static/backbone/ReferralsModule.js
+++ refrew/static/backbone/ReferralsModule.js
@@ -228,12 +228,7 @@ if (!this.gmm || typeof this.gmm !== 'object') {
 		},
 		submitRef : function(e){
 			e.preventDefault();
-			var loading = new LoadingView({
-				title: "Saving Reference",
-        			message: "Saving Reference"
-			});
-			Viewer.mainSub21.show(loading);
-                	var data = Backbone.Syphon.serialize(this);
+			var data = Backbone.Syphon.serialize(this);
 			console.log(data);
 			var model = new LinkedInSearchResultModel(data);
                 	model.save();
@@ -286,39 +281,6 @@ if (!this.gmm || typeof this.gmm !== 'object') {
 
 	/* Spinner */
 	
-	var LoadingView = Marionette.ItemView.extend({
-    		template: "#loading-view",
-		tagName: 'div',
-		className: 'col-lg-6',
-    		serializeData: function(){
-      		return {
-        		title: this.options.title || "Loading Data",
-        		message: this.options.message || "Please wait, data is loading."
-     		 }
-    		},
-    		onShow: function(){
-      			var opts = {
-        		lines: 13, // The number of lines to draw
-        		length: 10, // The length of each line
-        		width: 5, // The line thickness
-        		radius: 10, // The radius of the inner circle
-        		corners: 1, // Corner roundness (0..1)
-        		rotate: 0, // The rotation offset
-        		direction: 1, // 1: clockwise, -1: counterclockwise
-        		color: '#000', // #rgb or #rrggbb
-        		speed: 1, // Rounds per second
-        		trail: 60, // Afterglow percentage
-        		shadow: false, // Whether to render a shadow
-        		hwaccel: false, // Whether to use hardware acceleration
-        		className: 'spinner', // The CSS class to assign to the spinner
-        		zIndex: 2e9, // The z-index (defaults to 2000000000)
-       	 		/*top: '10px', // Top position relative to parent in px
-        		left: '10px' // Left position relative to parent in px*/
-      			};
-      			$('#spinner').spin(opts);
-    	     }
-  	});
-
 	/* End */
 	
 });	
